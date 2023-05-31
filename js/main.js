0aa25e827f50f51b996ae594e0c876acf8fe132f
Maria2009gift <kovalenkom2009@gmail.com>
2023-05-31 18:50:07 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -3,24 +3,38 @@
 const age = 14
 console.log(age)
 
+// -----------------
+
 let name = "Maria"
 console.log(name)
+
+// -----------------
 
 const isStudent=true
 console.log("isStudent:>>", isStudent)
 
+// -----------------
+
 let myString = "я люблю піцу" 
 console.log(myString)
+
+// -----------------
 
 const myNumber = 14
 const myNumb = myNumber + 10
 console.log(myNumb)
 
+// -----------------
+
 const myNull = null
 console.log(myNull)
 
+// -----------------
+
 const Name = prompt("Введіть своє ім'я");
 console.log("Добрий день, ", Name);
+
+// -----------------
 
 const isComing = confirm("Натисніть ok або Cancel");
 if (isComing == false) {
@@ -29,6 +43,8 @@ if (isComing == false) {
 if (isComing == true) {
     console.log("Дякую за підтвердження!")
 }
+
+// -----------------
 
 const danger = "Дія небезпечна, підтвердіть її або відхиліть!";
 alert(danger)
